4a2e6603b9d4dcd114e93d032f7ef887fbcbb80d
Josh Batchelor <josh_batch@live.com.au>
2015-02-01 02:52:11 +0800

    Start of paragraph highlighting. Stopping because 3am.

--- public/js/a-deeper-reds.js
+++ public/js/a-deeper-reds.js
@@ -3,17 +3,29 @@ var wordArray = [];
 var i = 0;
 var playing = false;
 var pausePeriod = 1.5;
+var pauseComma = 0.5;
 
 var wpm = 300;
 
 var timer;
+
+// This is pretty great. Thankyou beiyuu
+// https://gist.github.com/beiyuu/2029907
+$.fn.selectRange = function(start, end) {
+    var e = document.getElementById($(this).attr('id')); // I don't know why... but $(this) don't want to work today :-/
+    if (!e) return;
+    else if (e.setSelectionRange) { e.focus(); e.setSelectionRange(start, end); } /* WebKit */ 
+    else if (e.createTextRange) { var range = e.createTextRange(); range.collapse(true); range.moveEnd('character', end); range.moveStart('character', start); range.select(); } /* IE */
+    else if (e.selectionStart) { e.selectionStart = start; e.selectionEnd = end; }
+};
+
 
 function updateText(){
 
 }
 
 function stepBackward(){
-
+	highlightParagraph(2);
 }
 
 function stepForward(){
@@ -36,8 +48,14 @@ function stepForward(){
 function play(){
 	if(!playing)
 	{
+		
+
 		console.log('play');
 		text = $('#text').val();
+
+		var paraArray = text.split('\n');
+		console.log(cleanArray(paraArray));
+
 		wordArray = text.split(' ');
 		i = 0;
 
@@ -82,4 +100,59 @@ function colorLetter(){
 
 	wordArray[i] = beg + letter + end;
 }
+
+function cleanArray(array){
+	var newArray = [];
+	for(var index = 0; index < array.length; index++){
+		if(array[index]){
+			newArray.push(array[index]);
+		}
+	}
+
+	return newArray;
+}
+
+function highlightParagraph(index){
+	//find paragraph
+	var curr = 0;
+	var newLineCount = 0;
+	var start = 0;
+	var end = 0;
+	var currChar;
+	var text = $('#text').val() + '\n';
+	
+	while(newLineCount < index){
+		console.log(curr);
+		currChar = text.charAt(curr);
+		if(currChar == '\n'){
+			newLineCount++;
+		}
+		curr++;
+	}
+	start = curr;
+	console.log('new');
+	while(newLineCount == index){
+		console.log(curr);
+		currChar = text.charAt(curr);
+		if(currChar == '\n'){
+			end = curr;
+			newLineCount++;
+		}
+		curr++;
+	}
+
+	// console.log(start);
+	// console.log(end);
+	$('#text').selectRange(start, end);
+}
+
+
+
+
+
+
+
+
+
+
 
